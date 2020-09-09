333fc801b9afeb76e22dab2383362a8c16e1cd62
Pit Wegner <pit.wegner@bdr.de>
2020-09-09 18:05:14 +0200

    remove secret django key from settings.py

--- mpttexport/settings.py
+++ mpttexport/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_f=j3rtyzvc9#3wt$^%nw)$9@zi8@s_q$b2_92ao*t!igr1130'
+SECRET_KEY = os.environ.get('SECRET_KEY') #'_f=j3rtyzvc9#3wt$^%nw)$9@zi8@s_q$b2_92ao*t!igr1130'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
